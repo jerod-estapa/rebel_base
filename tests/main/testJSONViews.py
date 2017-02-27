0e66d28c358f1180a117c99d9601d7ce4b5653df
Jerod Estapa <jerodestapa@gmail.com>
2017-02-26 18:27:23 -0600

    Fixed json_views tests

--- tests/main/testJSONViews.py
+++ tests/main/testJSONViews.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from main.json_views import status_collection
+from main.json_views import StatusCollection
 from main.models import StatusReport
 from main.serializers import StatusReportSerializer
 
@@ -19,5 +19,5 @@ class JsonViewTests(TestCase):
     def test_get_collection(self):
         status = StatusReport.objects.all()
         expected_json = StatusReportSerializer(status, many=True).data
-        response = status_collection(DummyRequest('GET'))
+        response = StatusCollection.as_view()(DummyRequest('GET'))
         self.assertEqual(expected_json, response.data)
